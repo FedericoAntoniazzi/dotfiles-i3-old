6386fdd107e988dc9e4ee5a6c2ff4ae6311b082e
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2020-04-14 12:24:16 +0200

    Modify backlight using mouse wheel

--- .config/polybar/backlight.sh
+++ .config/polybar/backlight.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+# This polybar script show the current brightness level and allows you to increase or decrease it by scrolling up or down with the mouse wheel
+#
 # Make sure you have inotify-tools (and brightnessctl obv) installed otherwise inotifywait won't work
+# You need to install a font from nerdfonts.com in order to see glyphs correctly. You can replace them with what you prefer
 
 brightness_level=$(brightnessctl g)
 brightness_max=$(brightnessctl m)
@@ -24,5 +27,30 @@ getIcon() {
 	echo "$icon"
 }
 
-inotifywait -q -m -e close_write /sys/class/backlight/intel_backlight/brightness | 
-	echo "$(getIcon) $brightness_percent%"
+increase(){
+	brightnessctl s +2% > /dev/null
+	return
+}
+
+decrease(){
+	brightnessctl s 2%- > /dev/null
+	return
+}
+
+
+case "$1" in 
+	"increase")
+		increase
+		exit
+		;;
+	"decrease")
+		decrease
+		exit
+		;;
+	*)
+		inotifywait -q -m -e close_write /sys/class/backlight/intel_backlight/brightness | 
+			echo "$(getIcon) $brightness_percent%"
+		;;
+esac
+
+
